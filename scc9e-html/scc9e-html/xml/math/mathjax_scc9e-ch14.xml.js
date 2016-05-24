0b70a7f5ff0183fb6f4b81817cab98b75b400eff
andynewcomb <andy.nukem@gmail.com>
2016-05-24 16:58:07 -0700

    mathml removed from XML and put into js file

---
+++ scc9e-html/scc9e-html/xml/math/mathjax_scc9e-ch14.xml.js
@@ -0,0 +1,59 @@
+var MathJaxMap = {};
+
+
+MathJaxMap['math_1'] = '<math display=\'inline\'><mrow><mover accent=\'true\'><mi>x</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover><mo>=</mo><mn>58.2</mn><mtext>&#x2009;</mtext><mtext>cm</mtext></mrow></math>';
+MathJaxMap['math_2'] = '<math display=\'inline\'><mrow><msub><mstyle displaystyle=\'true\'><mi>s</mi></mstyle><mi>x</mi></msub><mo>=</mo><mn>13.20</mn><mtext>&#x2009;</mtext><mtext>&#x200B;</mtext><mtext>cm</mtext></mrow></math>';
+MathJaxMap['math_3'] = '<math display=\'inline\'><mrow><mover accent=\'true\'><mi>y</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover><mo>=</mo><mn>66.0</mn><mtext>&#x2009;</mtext><mtext>cm</mtext></mrow></math>';
+MathJaxMap['math_4'] = '<math display=\'inline\'><mrow><msub><mstyle displaystyle=\'true\'><mi>s</mi></mstyle><mi>y</mi></msub><mo>=</mo><mn>15.89</mn><mtext>&#x2009;</mtext><mtext>&#x200B;</mtext><mtext>cm</mtext></mrow></math>';
+MathJaxMap['math_5'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mi>x</mi><mo>&#x2212;</mo><mover accent=\'true\'><mi>x</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover></mrow><mo>)</mo></mrow><mo>/</mo><msub><mstyle displaystyle=\'true\'><mi>s</mi></mstyle><mi>x</mi></msub></mrow></math>';
+MathJaxMap['math_6'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mi>y</mi><mo>&#x2212;</mo><mover accent=\'true\'><mi>y</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover></mrow><mo>)</mo></mrow><mo>/</mo><msub><mstyle displaystyle=\'true\'><mi>s</mi></mstyle><mi>y</mi></msub></mrow></math>';
+MathJaxMap['math_7'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>38</mn><mo>&#x2212;</mo><mn>58.2</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>13.20</mn><mo>=</mo><mo>&#x2212;</mo><mn>1.530</mn></mrow></math>';
+MathJaxMap['math_8'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>41</mn><mo>&#x2212;</mo><mn>66.0</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>15.89</mn><mo>=</mo><mo>&#x2212;</mo><mn>1.573</mn></mrow></math>';
+MathJaxMap['math_9'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>56</mn><mo>&#x2212;</mo><mn>58.2</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>13.20</mn><mo>=</mo><mo>&#x2212;</mo><mn>0.167</mn></mrow></math>';
+MathJaxMap['math_10'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>63</mn><mo>&#x2212;</mo><mn>66.0</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>15.89</mn><mo>=</mo><mo>&#x2212;</mo><mn>0.189</mn></mrow></math>';
+MathJaxMap['math_11'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>59</mn><mo>&#x2212;</mo><mn>58.2</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>13.20</mn><mo>=</mo><mtext>&#x00A0;</mtext><mn>0.061</mn></mrow></math>';
+MathJaxMap['math_12'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>70</mn><mo>&#x2212;</mo><mn>66.0</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>15.89</mn><mo>=</mo><mtext>&#x00A0;</mtext><mn>0.252</mn></mrow></math>';
+MathJaxMap['math_13'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>64</mn><mo>&#x2212;</mo><mn>58.2</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>13.20</mn><mo>=</mo><mtext>&#x00A0;</mtext><mn>0.439</mn></mrow></math>';
+MathJaxMap['math_14'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>72</mn><mo>&#x2212;</mo><mn>66.0</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>15.89</mn><mo>=</mo><mtext>&#x00A0;</mtext><mn>0.378</mn></mrow></math>';
+MathJaxMap['math_15'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>74</mn><mo>&#x2212;</mo><mn>58.2</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>13.20</mn><mo>=</mo><mtext>&#x00A0;</mtext><mn>1.197</mn></mrow></math>';
+MathJaxMap['math_16'] = '<math display=\'inline\'><mrow><mrow><mo>(</mo><mrow><mn>84</mn><mo>&#x2212;</mo><mn>66.0</mn></mrow><mo>)</mo></mrow><mo>/</mo><mn>15.89</mn><mo>=</mo><mtext>&#x00A0;</mtext><mn>1.133</mn></mrow></math>';
+MathJaxMap['math_17'] = '<math display=\'block\'><mtable columnalign=\'left\'><mtr columnalign=\"left\"><mtd columnalign=\'right\'><mstyle displaystyle=\'true\'><mi>r</mi></mstyle></mtd><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mo>=</mo></mstyle></mtd><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mfrac><mn>1</mn><mn>4</mn></mfrac><mrow><mo>[</mo><mrow><mrow><mo>(</mo><mrow><mo>&#x2212;</mo><mn>1.530</mn></mrow><mo>)</mo></mrow><mrow><mo>(</mo><mrow><mo>&#x2212;</mo><mn>1.573</mn></mrow><mo>)</mo></mrow><mo>+</mo></mrow></mrow><mrow><mo>(</mo><mrow><mo>&#x2212;</mo><mn>0.167</mn></mrow><mo>)</mo></mrow><mrow><mo>(</mo><mrow><mo>&#x2212;</mo><mn>0.189</mn></mrow><mo>)</mo></mrow><mo>+</mo><mrow><mo>(</mo><mrow><mn>0.061</mn></mrow><mo>)</mo></mrow><mrow><mo>(</mo><mrow><mn>0.252</mn></mrow><mo>)</mo></mrow></mstyle></mtd></mtr><mtr columnalign=\"left\"><mtd columnalign=\'right\'/><mtd columnalign=\'left\'/><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mrow><mrow><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mtext>&#x2009;</mtext><mo>+</mo><mrow><mo>(</mo><mrow><mn>0.439</mn></mrow><mo>)</mo></mrow><mrow><mo>(</mo><mrow><mn>0.378</mn></mrow><mo>)</mo></mrow><mo>+</mo><mrow><mo>(</mo><mrow><mn>1.197</mn></mrow><mo>)</mo></mrow><mrow><mo>(</mo><mrow><mn>1.133</mn></mrow><mo>)</mo></mrow></mrow><mo>]</mo></mrow></mstyle></mtd></mtr><mtr columnalign=\"left\"><mtd columnalign=\'right\'/><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mo>=</mo></mstyle></mtd><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mfrac><mn>1</mn><mn>4</mn></mfrac><mtext>(2</mtext><mtext>.4067&#x00A0;+&#x00A0;0</mtext><mtext>.0316&#x00A0;+&#x00A0;0</mtext><mtext>.0154&#x00A0;+&#x00A0;0</mtext><mtext>.1659&#x00A0;+&#x00A0;1</mtext><mtext>.3562)</mtext></mstyle></mtd></mtr><mtr columnalign=\"left\"><mtd columnalign=\'right\'/><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mo>=</mo></mstyle></mtd><mtd columnalign=\'left\'><mstyle displaystyle=\'true\'><mfrac><mrow><mn>3.9758</mn></mrow><mn>4</mn></mfrac><mo>=</mo><mn>0.994</mn></mstyle></mtd></mtr></mtable></math>';
+MathJaxMap['math_18'] = '<math display=\'block\'><mi>r</mi><mo>=</mo><mfrac><mn>1</mn><mrow><mi>n</mi><mo>&#x2212;</mo><mn>1</mn></mrow></mfrac><mstyle displaystyle=\'true\'><mo>&#x2211;</mo><mrow><mrow><mo>(</mo><mrow><mfrac><mrow><mi>x</mi><mo>&#x2212;</mo><mover accent=\'true\'><mi>x</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover></mrow><mrow><msub><mstyle displaystyle=\'true\'><mi>s</mi></mstyle><mi>x</mi></msub></mrow></mfrac></mrow><mo>)</mo></mrow><mrow><mo>(</mo><mrow><mfrac><mrow><mi>y</mi><mo>&#x2212;</mo><mover accent=\'true\'><mi>y</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover></mrow><mrow><msub><mstyle displaystyle=\'true\'><mi>s</mi></mstyle><mi>y</mi></msub></mrow></mfrac></mrow><mo>)</mo></mrow></mrow></mstyle></math>';
+MathJaxMap['math_19'] = '<math display=\'inline\'><mo>&#x2211;</mo></math>';
+MathJaxMap['math_20'] = '<math display=\'inline\'><mrow><mover accent=\'true\'><mi>x</mi><mo stretchy=\'true\'>&#x00AF;</mo></mover></mrow></math>';
+MathJaxMap['math_21'] = '<math display=\'inline\'><mi>r</mi></math>';
+
+
+
+for (var key in MathJaxMap) {
+  
+  if (MathJaxMap.hasOwnProperty(key)) {
+    $('[data-math=' + key + ']').html(MathJaxMap[key]);
+  }
+}
+
+$.ajaxSetup({
+  cache: true
+});
+
+//configure the mathjax engine
+window.MathJax = {
+	"HTML-CSS": {
+		mtextFontInherit: true,
+		scale: 98,
+		minScaleAdjust: 100,
+		noReflows:false 
+	},
+	MathML: {
+		useMathMLspacing: false
+	},
+	menuSettings: {
+		zoom: "Click"	
+	},
+	MathMenu: {
+		showFontMenu: true
+	}
+	
+  };
+
+$.getScript( "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"); 
